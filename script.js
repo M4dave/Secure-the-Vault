3052d5f6104dc09f722f5c3bada2825e84ea8b09
M4dave <davidjohncruz@gmail.com>
2024-03-12 22:45:50 -0400

    updated files

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//will create a const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//will create 3 const using the math floor math random
+//will create script.js and will add all my const inside the script.js
+//will create getElementById innerHTML to call all my const to display it on my HTML 
+ 
+ 
  // created a 3 const and Math random to display different number
 const firstNum = Math.floor(Math.random()*100); 
 const secondNum = Math.floor(Math.random()*100);
@@ -11,7 +17,3 @@ console.log(thirdNum);
 console.log(myGreeting);
 // created a getElementById innerHTML to call all my const to display it on my HTML
 document.getElementById("myGreeting").innerHTML = myGreeting + " " + firstNum + secondNum + thirdNum;
-
-//will create a const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
-//will create 3 const using addition the math floor math random
-//will create script.js and will add all my const inside the script.js
