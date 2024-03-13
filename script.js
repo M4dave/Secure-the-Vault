760062be9cdfd4b144c08711d1fa75cdcf5ff797
M4dave <davidjohncruz@gmail.com>
2024-03-12 22:44:52 -0400

    updated files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  // created a 3 const and Math random to display different number
-const firstNum = Math.floor(Math.random()*10); 
-const secondNum = Math.floor(Math.random()*10);
-const thirdNum = Math.floor(Math.random()*10);
+const firstNum = Math.floor(Math.random()*100); 
+const secondNum = Math.floor(Math.random()*100);
+const thirdNum = Math.floor(Math.random()*100);
 // created a const for string
 const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"; 
 // created a console log to print out my const
@@ -13,5 +13,5 @@ console.log(myGreeting);
 document.getElementById("myGreeting").innerHTML = myGreeting + " " + firstNum + secondNum + thirdNum;
 
 //will create a const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
-//will create 3 const using addition minus and multiplcation, for example const addNum = 3 + 4; const minusNum = 3 - 4; and const multiplyNum = 3 * 8;
+//will create 3 const using addition the math floor math random
 //will create script.js and will add all my const inside the script.js
