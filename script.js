ff1521dc5eb653e91d9fff3fcd8c4c228bae82c3
M4dave <davidjohncruz@gmail.com>
2024-03-12 21:58:57 -0400

    updated files

---
+++ script.js
@@ -0,0 +1,12 @@
+const firstNum = 3 + 8;
+const secondNum = 15 - 1;
+const thirdNum = 4 * 5;
+const vault = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+console.log(firstNum);
+console.log(secondNum);
+console.log(thirdNum);
+
+//will create a const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//will create 3 const using addition minus and multiplcation, for example const addNum = 3 + 4; const minusNum = 3 - 4; and const multiplyNum = 3 * 8;
+//will create script.js and will add all my const inside the script.js
