75ccb55f5e7c4164848a31af97c485ef49d1bb3f
M4dave <davidjohncruz@gmail.com>
2024-03-12 22:30:03 -0400

    updated files

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ console.log(secondNum);
 console.log(thirdNum);
 console.log(myGreeting);
 // created a getElementById innerHTML to call all my const to display it on my HTML
-document.getElementById("myGreeting").innerHTML = myGreeting;
-document.getElementById("firstCode").innerHTML = "The first Code is " + firstNum;
-document.getElementById("secondCode").innerHTML = "The first Code is " + secondNum;
-document.getElementById("thirdCode").innerHTML = "The first Code is " + thirdNum;
+document.getElementById("myGreeting").innerHTML = myGreeting + " " + firstNum + secondNum + thirdNum;
 
 //will create a const myGreeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
 //will create 3 const using addition minus and multiplcation, for example const addNum = 3 + 4; const minusNum = 3 - 4; and const multiplyNum = 3 * 8;
